7c42d42537b22639a40659c68b95c0bd1388d923
bd-git <bduncan1125@gmail.com>
2014-07-22 09:06:31 -0400

    Initial

---
+++ EnableEnterpriseMode/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace EnableEnterpriseMode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.emBox = new System.Windows.Forms.CheckBox();
+            this.inputLink = new System.Windows.Forms.TextBox();
+            this.ieVerLabel = new System.Windows.Forms.Label();
+            this.disableLabel = new System.Windows.Forms.Label();
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.siteLabel = new System.Windows.Forms.Label();
+            this.helpButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // emBox
+            // 
+            this.emBox.AutoSize = true;
+            this.emBox.Location = new System.Drawing.Point(15, 109);
+            this.emBox.Name = "emBox";
+            this.emBox.Size = new System.Drawing.Size(306, 17);
+            this.emBox.TabIndex = 0;
+            this.emBox.Text = "Enable (Launch IE then navigate to site. Disable to Unlock)";
+            this.emBox.UseVisualStyleBackColor = true;
+            this.emBox.CheckedChanged += new System.EventHandler(this.emBox_CheckChanged);
+            // 
+            // inputLink
+            // 
+            this.inputLink.Location = new System.Drawing.Point(15, 83);
+            this.inputLink.Name = "inputLink";
+            this.inputLink.Size = new System.Drawing.Size(302, 20);
+            this.inputLink.TabIndex = 1;
+            this.inputLink.ReadOnlyChanged += new System.EventHandler(this.inputLink_ReadOnlyChanged);
+            // 
+            // ieVerLabel
+            // 
+            this.ieVerLabel.AutoSize = true;
+            this.ieVerLabel.Location = new System.Drawing.Point(15, 31);
+            this.ieVerLabel.Name = "ieVerLabel";
+            this.ieVerLabel.Size = new System.Drawing.Size(0, 13);
+            this.ieVerLabel.TabIndex = 2;
+            // 
+            // disableLabel
+            // 
+            this.disableLabel.AutoSize = true;
+            this.disableLabel.Location = new System.Drawing.Point(323, 86);
+            this.disableLabel.Name = "disableLabel";
+            this.disableLabel.Size = new System.Drawing.Size(0, 13);
+            this.disableLabel.TabIndex = 3;
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.AutoSize = true;
+            this.instructionsLabel.Location = new System.Drawing.Point(15, 9);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(365, 13);
+            this.instructionsLabel.TabIndex = 4;
+            this.instructionsLabel.Text = "You must verify IE Version is atleast 11. Click help button for program usage.";
+            // 
+            // siteLabel
+            // 
+            this.siteLabel.AutoSize = true;
+            this.siteLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.siteLabel.Location = new System.Drawing.Point(15, 58);
+            this.siteLabel.Name = "siteLabel";
+            this.siteLabel.Size = new System.Drawing.Size(375, 13);
+            this.siteLabel.TabIndex = 5;
+            this.siteLabel.Text = "Site must be domain and subdomains only e.g. google.com or play.google.com";
+            // 
+            // helpButton
+            // 
+            this.helpButton.Location = new System.Drawing.Point(419, 3);
+            this.helpButton.Name = "helpButton";
+            this.helpButton.Size = new System.Drawing.Size(27, 24);
+            this.helpButton.TabIndex = 6;
+            this.helpButton.Text = "?";
+            this.helpButton.UseVisualStyleBackColor = true;
+            this.helpButton.Click += new System.EventHandler(this.helpButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 138);
+            this.Controls.Add(this.helpButton);
+            this.Controls.Add(this.siteLabel);
+            this.Controls.Add(this.instructionsLabel);
+            this.Controls.Add(this.disableLabel);
+            this.Controls.Add(this.ieVerLabel);
+            this.Controls.Add(this.inputLink);
+            this.Controls.Add(this.emBox);
+            this.Name = "Form1";
+            this.Text = "Enable Enterprise Mode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox emBox;
+        private System.Windows.Forms.TextBox inputLink;
+        private System.Windows.Forms.Label ieVerLabel;
+        private System.Windows.Forms.Label disableLabel;
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.Label siteLabel;
+        private System.Windows.Forms.Button helpButton;
+    }
+}
+
---
+++ EnableEnterpriseMode/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.IO;
+
+namespace EnableEnterpriseMode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (get_XML())
+            {
+                if (getEM() == fileName())
+                {
+                    emBox.Checked = true;
+                    inputLink.ReadOnly = true;
+                }
+                else
+                {
+                    inputLink.Text = "";
+                }
+            }
+
+            string ieVersion = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Internet Explorer").GetValue("Version").ToString();
+            string[] sep = new string[] { "." };
+            string[] result = ieVersion.Split(sep, StringSplitOptions.None);
+            ieVerLabel.Text = "Version of IE installed: " + result[1];
+        }
+
+        private void emBox_CheckChanged(object sender, EventArgs e)
+        {
+            if (emBox.Checked == true)
+            {
+                if (!setEM()) //if setting the Enterprise Mode Reg fails, uncheck the box
+                {
+                    emBox.Checked = false;
+                }
+            }
+            else //else, if box is unchecked, to try delete the key:
+            {
+                try
+                {
+                    inputLink.ReadOnly = false;
+                    disableLabel.Text = "";
+                    Microsoft.Win32.Registry.CurrentUser.DeleteSubKeyTree(@"Software\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode");
+                }
+                catch { }
+            }
+        }
+
+        private bool get_XML()
+        {
+            bool ret = false;
+            try
+            {
+                XElement xml = XElement.Load(fileName()).Element("emie").Element("domain");
+                inputLink.Text = xml.Value.ToString();
+                ret = true;
+            }
+            catch { }//could not load xml file
+            return ret;
+        }
+
+        private bool set_XML()
+        {
+            XElement rules = new XElement("rules");
+            rules.SetAttributeValue("version","1");
+
+            XElement em = new XElement("emie");
+
+            XElement domain = new XElement("domain",inputLink.Text);
+            domain.SetAttributeValue("exclude","false");
+
+            em.Add(domain);
+            rules.Add(em);
+
+            XDocument doc = new XDocument(rules);
+
+            //doc.Save(fileName); //We can do this because EM doesn't accept the xml it generates
+            System.IO.File.WriteAllText(fileName(), doc.ToString()); //it does accept this though
+
+            return true;
+        }
+
+        private string getEM()
+        {
+            string emVal = "";
+            try
+            {
+                Microsoft.Win32.RegistryKey key;
+                key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(@"Software\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode");
+                emVal = key.GetValue("SiteList").ToString();
+                key.Close();
+            }
+            catch { }
+            return emVal;
+        }
+
+        private bool setEM()
+        {
+            bool ret = false;
+            try
+            {
+                Microsoft.Win32.RegistryKey key;
+                key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Policies\Microsoft\Internet Explorer\Main\EnterpriseMode");
+                key.SetValue("Enable", "");
+                key.SetValue("SiteList", fileName());
+                key.Close();
+                ret = set_XML();
+                inputLink.ReadOnly = ret;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("The system is showing that you do not have Administrator Access.\n\nPlease check with your system admin.");
+            }
+            return ret;
+        }
+
+        private string fileName()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "sitelist.xml");
+        }
+
+        private void inputLink_ReadOnlyChanged(object sender, EventArgs e)
+        {
+            if (inputLink.ReadOnly == true)
+            {
+                disableLabel.Text = "Locked";
+            }
+            else
+            {
+                disableLabel.Text = "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void helpButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Enable EnterpriseMode (google it...)\n\n1)Enter Site Into Textbox\n2)Click Enable\n3)Open Internet Explorer\n4)Navigate to Website\n\nUnfortunately, IE will not detect a site change until it detects that it's been disabled then redetects that it's been enabled. To update the site you must:\n1)Disable\n2)Close all open IE windows then reopen IE\n3)Close the newly opened IE window (now it is ready to detect your new site)\n4)Add site and enable\n5)Re-open IE.\n\nbduncan1125@gmail.com");
+        }
+
+    }
+}
